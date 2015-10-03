4613f15f0f8ffea395890df58ac92d9136901ef6
shenry <shenry@handycodejob.com>
2015-10-03 15:23:06 -0700

    fix import error for docs

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ project_root = os.path.dirname(cwd)
 # version is used.
 sys.path.insert(0, project_root)
 
-import state-neighbors
+import state_neighbors
 
 # -- General configuration ---------------------------------------------
 
